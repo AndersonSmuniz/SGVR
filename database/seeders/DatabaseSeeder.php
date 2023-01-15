dc4fec9c26209854a303e5f1f263cfd3cc7da482
AndersonSmuniz <anderson.muniz100@hotmail.com>
2023-01-15 16:03:39 -0300

    TipoUsuariosSeeder acrescentado

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([UsuarioSeeder::class]);
+        $this->call([TipoUsuarioSeeder::class,
+            UsuarioSeeder::class
+        ]);
+
     }
 }
