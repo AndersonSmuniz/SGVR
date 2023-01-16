31150c0ad3323b19bbb1a6b3fd435cf2681074f5
Edgarvital <edgar.tecnico@hotmail.com>
2023-01-15 23:46:18 -0300

    Criar Migrate de item_pedido

---
+++ database/migrations/2023_01_16_023747_create_item_pedidos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_pedidos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->string('observacao')->nullable();
+            $table->string('status');
+            $table->foreignId('pedido_id')->constrained();
+            $table->foreignId('produto_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('itens_pedidos');
+    }
+};
