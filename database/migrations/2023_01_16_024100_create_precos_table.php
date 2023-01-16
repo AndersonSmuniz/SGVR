341ca394d484a356119ffd9cd322fa5156086582
AndersonSmuniz <anderson.muniz100@hotmail.com>
2023-01-16 00:05:26 -0300

    Criado Migrate de Preco

---
+++ database/migrations/2023_01_16_024100_create_precos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('precos', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('produto_id');
+            $table->integer('numero_pessoas');
+            $table->integer('valor');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('precos');
+    }
+};
