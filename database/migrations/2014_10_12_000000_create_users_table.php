629d70d729b3ddb5b63f26d1ca0da084eb35ff18
GuilhermeGz <guilherme.dna06@gmail.com>
2023-01-15 23:13:08 -0300

    Ajustado migrate de User

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nome');
+            $table->string('password');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-
+            $table->string('telefone');
+            $table->string('cpf');
             $table->foreignId('tipo_usuario_id')->constrained();
 
             $table->rememberToken();
