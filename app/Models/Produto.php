8f44d1d4b14b443de46cb5a93424c9e37a52d00e
GuilhermeGz <guilherme.dna06@gmail.com>
2023-01-16 22:23:16 -0300

    Criado relacionamentos de Produto

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Produto extends Model
 {
     use HasFactory;
+
+    public function ingredientes(){
+        return $this->belongsToMany(Ingrediente::class, ProdutoIngrediente::class, 'produto_id');
+    }
+
+    public function cardapios(){
+        return $this->belongsToMany(Cardapio::class, ProdutoCardapio::class, 'produto_id');
+    }
+
+    public function pedidos(){
+        return $this->belongsToMany(Pedido::class, ItemPedido::class, 'produto_id')->withPivot(['status', 'observacao']);
+    }
+
+    public function precos(){
+        return $this->hasMany(Preco::class);
+    }
+
+    public function categoria(){
+        return $this->belongsTo(Categoria::class);
+    }
+
 }
