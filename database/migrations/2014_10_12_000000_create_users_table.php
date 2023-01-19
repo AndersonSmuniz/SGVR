534f4cf11cb84b289e65d9aef355d1d06ef26df2
Edgarvital <edgar.tecnico@hotmail.com>
2023-01-18 22:55:26 -0300

    SoftDelete na tabela de Usuário

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->softDeletes();
             $table->string('nome');
             $table->string('password');
             $table->string('email')->unique();
